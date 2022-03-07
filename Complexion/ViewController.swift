24b8fcda6501b60b9327e97c3aea42cfbb2d5966
Jose <daelhoser@gmail.com>
2022-03-07 11:50:22 -0800

    Create coordinator protocol

--- Complexion/ViewController.swift
+++ Complexion/ViewController.swift
@@ -7,6 +7,28 @@
 
 import UIKit
 
+protocol Coordinator: AnyObject {
+    var childCoordinators: [Coordinator] { set get }
+    var navigationController: UINavigationController { set get }
+    
+    func start()
+}
+
+final class RootCoordinator: Coordinator {
+    var childCoordinators: [Coordinator] = [Coordinator]()
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+    }
+    
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
